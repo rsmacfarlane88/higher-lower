7c4a7e2e261b0b018c08e1668fb751d36c1e0536
Ross MacFarlane <rsmacfarlane88@gmail.com>
2017-06-29 22:57:54 +0100

    Added main menu items and an activity spinner
    - Added start, how to play and about menu items
    - Added an activity spinner to the main menu to show when the game is loading at the start

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,19 +1,38 @@
 var testBootstrap = require('/test/tests');
 var tests = new testBootstrap.tests();
+var syncing = false;
 
 console.log("Tests enabled:"+tests.testsEnabled());
 if (tests.testsEnabled()){
 	tests.start();
 }
 else {
-	$.window.open();
-
-	var syncManager = Alloy.createController('sync');
+	$.spinner.show();
+	syncing = true;
+	var syncManager = Alloy.createController('sync', { success: syncComplete });
 	syncManager.sync();
+
+	$.window.open();
+}
+
+function syncComplete(){
+	$.spinner.hide();
+	$.menuItems.show();
+	syncing = false;
 }
 
 function startGame(e){
-	var gameController = Alloy.createController('game');
-	gameController.getView().open();
-	gameController.startGame();
+	if(!syncing){
+		var gameController = Alloy.createController('game');
+		gameController.getView().open();
+		gameController.startGame();
+	}
+}
+
+function showHelp(e){
+
+}
+
+function showAbout(e){
+	
 }
